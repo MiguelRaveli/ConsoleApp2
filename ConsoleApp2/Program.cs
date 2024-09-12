2c8d9fa69fb0f1f959c9f0bce112c0f82904a210
Raveli <miguelraveli123@gmail.com>
2024-09-12 00:59:50 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,10 @@
+﻿string[] arCountry = ["Brasil", "Alemanha", "Espanha"];
+//foreach (var country in arCountry)
+//{
+//    Console.WriteLine($"Pais a seguir: {country}");
+//}
+
+for(int i = 0; i < arCountry.Length; i++)
+{
+    Console.WriteLine(arCountry[i]);
+}
